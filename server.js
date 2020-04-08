b376eb5d94485b53f5709720baf6955353e8f920
Pete1701 <pete.pertongonas@pirron.one>
2020-04-09 01:17:49 +0200

    middleware

--- server.js
+++ server.js
@@ -2,11 +2,18 @@ const express = require('express');
 
 const server = express();
 
+server.use(express.json());
+
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
 //custom middleware
+
+server.use(logger);
+// server.use(validateUserId);
+// server.use(validateUser);
+// server.use(validatePost);
 
 function logger(req, res, next) {}
 
